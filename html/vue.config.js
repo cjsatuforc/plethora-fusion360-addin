86af29d89eef0078194463325e541b9979ab22f9
Camden Fullmer <camdenfullmer@gmail.com>
2018-09-15 21:11:09 -0400

    Update dependencies and fix config.

--- html/vue.config.js
+++ html/vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
             config.output.publicPath = ""
         }
         config.devtool = 'source-map'
-    }
+    },
+    baseUrl: process.env.NODE_ENV === 'production' ? "" : '/'
 }
